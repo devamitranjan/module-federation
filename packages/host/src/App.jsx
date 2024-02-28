7be421bec79345310a3926f9472ff659760c0143
Amit Ranjan <connectdevamit@gmail.com>
2024-02-29 02:32:04 +0530

    Added working webpack host and vite using webpack cli

---
+++ packages/host/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "viteRemote/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Webpack + React + Vite</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
